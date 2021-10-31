6b62fdd560c45e5d2bf599edd7c8af972b0be576
shanjiaming <shan_jiaming@sjtu.edu.cn>
2021-10-31 14:01:17 +0800

    maybe all passed

--- build.bash
+++ build.bash
@@ -1,6 +1,5 @@
-# this script is called when the judge is building your compiler.
-# no argument will be passed in.
-# shellcheck disable=SC2121
 set ff=UNIX
-mkdir runContain
-find ./src -name "*.java" -print0|xargs -0 javac -d runContain -cp ./src/antlr-4.9.2-complete.jar
+set -e
+mkdir -p bin
+find ./src -name *.java | javac -d bin -cp /ulib/java/antlr-4.7.2-complete.jar @/dev/stdin
+#cp ./builtin/builtin.s .
