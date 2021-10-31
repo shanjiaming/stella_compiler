8a79dc3161379550d6e9dba6ca88c520b8d3a35c
shanjiaming <shan_jiaming@sjtu.edu.cn>
2021-10-31 14:02:44 +0800

    maybe all passed

--- build.bash
+++ build.bash
@@ -1,5 +1,5 @@
 set ff=UNIX
 set -e
 mkdir -p bin
-find ./src -name *.java | javac -d bin -cp /ulib/java/antlr-4.7.2-complete.jar @/dev/stdin
+find ./src -name *.java | javac -d bin -cp /ulib/java/antlr-4.9.2-complete.jar @/dev/stdin
 #cp ./builtin/builtin.s .
